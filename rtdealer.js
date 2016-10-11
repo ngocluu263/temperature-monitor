e79e53e2e6c05cc422d8208befc8711c5887fbf0
Luu Nguyen <luunguyen@ccintegration.com>
2016-10-11 22:57:00 +0700

    Update

--- rtdealer.js
+++ rtdealer.js
@@ -38,7 +38,7 @@ function workerTask() {
     var workload = args[1].toString('utf8');
     var workload2 = args.toString('utf8');
 
-    console.log(JSON.stringify(args[1]));
+    console.log(args[1].toSource());
 
     if (workload === 'Fired!') {
       console.log('Completed: '+total+' tasks ('+dealer.identity+')');
